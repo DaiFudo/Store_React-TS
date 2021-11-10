7c77ce34f3535260d68a79ae1512d090482c2a52
Ilya Igorevich <dai.fudo@mail.ru>
2021-11-10 19:04:59 +0300

    Работа над Хранилищем Store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -14,9 +14,21 @@ import { ICardInfo } from "./interface/interfaceCard";
 
 class Store {
   IUser?: {};
+  password?: string;
+  nickname?: string;
+  id?: string;
+  admin?: boolean;
+  banned?: boolean;
 
   constructor() {
     makeAutoObservable(this);
+  }
+  getUser(IUser: IUser) {
+    this.nickname = IUser.user.nickname;
+    this.password = IUser.user.password;
+    this.id = IUser.user.id;
+    this.admin = IUser.user.admin;
+    this.banned = IUser.user.banned;
   }
 }
 export default new Store();
